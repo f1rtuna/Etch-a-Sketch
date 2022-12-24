50b8b5ab835485c1a945ac40a81d86105317af0e
Kil-Hwan <kilhwanpyun@gmail.com>
2022-12-24 04:07:22 -0500

    good enough

--- app.js
+++ app.js
@@ -2,27 +2,45 @@ const board = document.querySelector(".board");
 const erase = document.querySelector(".btn-dark")
 const blue_marker = document.querySelector(".btn-primary")
 const black_marker = document.querySelector(".btn-light");
+
 let color = "black"; 
+let size = 16;
+
+const sizeValue = document.getElementById('sizeValue')
+const sizeSlider = document.getElementById('sizeSlider')
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+  
+function changeSize(value) {
+    size = value;
+    updateSizeValue(size)
+    reloadGrid()
+}
+  
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function reloadGrid() {
+    deleteCells()
+    makeBoard(size, size)
+    drawBoard()
+}
+
 function makeBoard(rows, cols) {
     // rows cols initialized to be 1 in styles but changed here in fucntion to be parameters
     board.style.setProperty('--grid-rows', rows);
     board.style.setProperty('--grid-cols', cols);
-    console.log(board);
     for(c = 0; c < (rows*cols); c++){
         let cell = document.createElement("div");
         cell.className = "grid-item";
-        // cell.innerText = c+1;
-        // cell.addEventListener("mouseover", () =>{
-        //     cell.style.backgroundColor = color;
-        //     console.log(cell)
-        // });
         board.appendChild(cell);
     };
 };
 
 function deleteCells(){
     const cells = document.querySelectorAll(".grid-item");
-    console.log(cells);
     for(let i = 0; i < cells.length; i++){
         cells[i].style.backgroundColor = "white";
     }
@@ -37,17 +55,15 @@ function changeBlack(){
 }
 
 function drawBoard(){
-    console.log(color)
     const cells = document.querySelectorAll(".grid-item");
     for (let i = 0; i < cells.length; i++){
         cells[i].addEventListener("mouseover", () =>{
             cells[i].style.backgroundColor = color;
-            console.log(cells[i])
         });
     }
 }
-  
-makeBoard(16, 16);
+
+makeBoard(size, size);
 erase.addEventListener("click", deleteCells);
 blue_marker.addEventListener("click", changeBlue);
 black_marker.addEventListener("click", changeBlack);
